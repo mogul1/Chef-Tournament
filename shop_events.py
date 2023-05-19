9f6d959de90bbaf1065d0d0449a80ac1e5d14ed7
mogul1 <killabot0@gmail.com>
2023-05-19 16:06:51 +0800

    Create shop_events.py

---
+++ shop_events.py
@@ -0,0 +1,21 @@
+#Enters the shop where you can use your gold to buy things before a round starts.
+import questions as input_h
+import weapons as wep
+def shop():
+    print ("-------------------------------------------")
+    print ("You have  now entered the blacksmith's store.")
+    print ("-------------------------------------------")
+    while True:
+        x = input_h.int_input("You can either enter the store (by typing 1) or check your current equipment (by typing 2).")
+        if x == 1:
+            print ("You have entered the store.")
+            from main import menu
+            menu()
+        if x == 2:
+            print ("This is what your character currently has equipped.")
+            print("------------------------------------------------------")
+            wep.check_equipment()
+            print("------------------------------------------------------")
+            input("press any key to go back to the previous menu.")
+        else:
+            print ("1 or 2 only!")
